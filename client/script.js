ce37bca1b3e947fd8f14a9a010cadb4079d09643
shanthosh04 <shanthosh1610@gmail.com>
2023-11-11 16:28:00 +0100

    frotend aufbau

--- client/script.js
+++ client/script.js
@@ -1,18 +1,48 @@
+let username;
+const messagesContainer = document.getElementById('messages');
+const usernameInput = document.getElementById('username');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('send');
 const socket = new WebSocket("ws://localhost:3000");
 
-socket.addEventListener("open", (event) => {
+usernameInput.addEventListener('change', (event) => {
+  username = event.target.value;
+  messageInput.disabled = !username;
+});
+
+socket.addEventListener('open', (event) => {
   console.log("WebSocket connected!");
-  socket.send("Hello, server!");
 });
 
-socket.addEventListener("message", (event) => {
-  console.log(`Received message: ${event.data}`);
+socket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
+  displayMessage(data.username, data.message);
 });
 
-socket.addEventListener("close", (event) => {
+socket.addEventListener('close', (event) => {
   console.log("WebSocket closed.");
 });
 
-socket.addEventListener("error", (event) => {
+socket.addEventListener('error', (event) => {
   console.error("WebSocket error:", event);
 });
+
+sendButton.addEventListener('click', sendMessage); 
+
+function sendMessage() {
+  if (messageInput.value.trim() === '' || !username) {
+    alert('Please enter a username and a message.');
+    return;
+  }
+  const message = messageInput.value;
+  socket.send(JSON.stringify({ username, message }));
+  displayMessage(username, message);
+  messageInput.value = '';
+}
+
+function displayMessage(user, message) {
+  const messageElement = document.createElement('div');
+  messageElement.textContent = `${user}: ${message}`;
+  messagesContainer.appendChild(messageElement);
+  messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
